6747480be000d51262e9ecee0d50088cce6b41c6
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-27 20:09:42 -0300

    Criacao do projeto com Spring Initializr

---
+++ ProjetoSGV/ProjetoSGV/src/main/java/br/com/fiap/cp01/ProjetoSGV/ProjetoSgvApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cp01.ProjetoSGV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSgvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSgvApplication.class, args);
+	}
+
+}
---
+++ ProjetoSGV/ProjetoSGV/src/test/java/br/com/fiap/cp01/ProjetoSGV/ProjetoSgvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cp01.ProjetoSGV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSgvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
