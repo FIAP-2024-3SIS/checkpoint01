f22a5b16532e49f81b0f17a510e34e17e1dd71d0
luauberths <124469416+luauberths@users.noreply.github.com>
2024-03-28 18:04:05 -0300

    Update ProjetoSgvApplication.java
    Código Main da aplicação.

--- ProjetoSGV/ProjetoSGV/src/main/java/br/com/fiap/cp01/ProjetoSGV/ProjetoSgvApplication.java
+++ ProjetoSGV/ProjetoSGV/src/main/java/br/com/fiap/cp01/ProjetoSGV/ProjetoSgvApplication.java
@@ -1,14 +1,28 @@
 package br.com.fiap.cp01.ProjetoSGV;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import dto.ViagemDTO;
 
-@SpringBootApplication
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
 public class ProjetoSgvApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoSgvApplication.class, args);
-		
+//      SpringApplication.run(ProjetoSgvApplication.class, args);
+
+	// Ajustar distância ou velocidadeMedia conforme necessário
+	double distancia = 160.0;
+	int velocidadeMedia = 100;
+
+	// Cria o objeto ViagemDTO
+	ViagemDTO viagem = new ViagemDTO(distancia, velocidadeMedia);
+
+	// Chama o método e armazena o resultado
+	String resultado = viagem.calculaTempoViagem();
+
+	// Exibe o resultado
+	System.out.println(resultado);
 		
 	}
 
