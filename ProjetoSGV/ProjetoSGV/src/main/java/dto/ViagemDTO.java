38e2647bc9ffe99efa4484d92148caa7f0afe65e
ormart <ormartnicolas@gmail.com>
2024-03-28 13:39:08 -0300

    Criação metodo para calculo do tempo de viagem

--- ProjetoSGV/ProjetoSGV/src/main/java/dto/ViagemDTO.java
+++ ProjetoSGV/ProjetoSGV/src/main/java/dto/ViagemDTO.java
@@ -2,9 +2,24 @@ package dto;
 
 public class ViagemDTO {
 
-	private double Distancia;
-	private int VelocidadeMedia;
-	private double Duracao;
-	
-	
+	private double distancia;
+	private int velocidadeMedia;
+
+	public ViagemDTO(double distancia, int velocidadeMedia) {
+		this.distancia = distancia;
+		this.velocidadeMedia = velocidadeMedia;
+	}
+
+	public String calculaTempoViagem() {
+
+		// Tempo em horas
+		double tempoEmHoras = this.distancia / this.velocidadeMedia;
+
+		// Horas e minutos
+		int horas = (int) tempoEmHoras;
+		int minutos = (int) ((tempoEmHoras - horas) * 60);
+
+		return "O tempo de viagem é: " + horas + "horas e " + minutos + " minutos.";
+	}
+
 }
